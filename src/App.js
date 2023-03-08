b2b0f3882d5835dd0bd05c628414619b0c5c2500
diegogirao <diegoramiro25@gmail.com>
2023-03-08 18:39:39 -0300

    Feat add components home and details

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Details from "./pages/Details"
+import Home from "./pages/Home"
+
+function App() {
+	return (
+		<div className="header">
+			<Routes>
+				<Route path="/details" element={<Details />} />
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</div>
+	)
+}
+
+export default App
