11e7728ba2fa744f0fc626008e7bb3b0c56f0e74
diegogirao <diegoramiro25@gmail.com>
2024-03-07 21:35:38 +0000

    feat: import header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Details from "./pages/Details"
 import Home from "./pages/Home"
+import Header from "./components/Header"
 
 function App() {
 	return (
 		<div className="header">
+			<Header />
 			<Routes>
 				<Route path="/details" element={<Details />} />
 				<Route path="/" element={<Home />} />
