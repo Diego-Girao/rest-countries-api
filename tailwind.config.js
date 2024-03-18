11cfdbadd84efa2a11fd6456db1e9a4e8bc4fae8
Diego <diegoramiro25@gmail.com>
2024-03-18 20:57:48 +0000

    chore: change to default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,6 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: "class",
 	theme: {
-		screens: {
-			mobile: "340px",
-			// => @media (min-width: 340px) { ... }
-
-			tablet: "600px",
-			// => @media (min-width: 600px) { ... }
-
-			laptop: "1024px",
-			// => @media (min-width: 1024px) { ... }
-
-			desktop: "1280px",
-			// => @media (min-width: 1280px) { ... }
-		},
-	},
-	variants: {
 		extend: {},
 	},
 	plugins: [],
