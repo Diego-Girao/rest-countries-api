c74e1dce61405ef3bfe1f806c782dc06f448a5b8
Diego <diegoramiro25@gmail.com>
2024-03-18 21:01:04 +0000

    style: change custom breakpoint to default and positions

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -14,24 +14,24 @@ function Details(props) {
 		<div className="bg-gray-100 dark:bg-gray-800 dark:text-white">
 			<div className="container mx-auto mb-2">
 				<button
-					className="px-8 py-2 bg-white text-gray-600 shadow-md rounded-lg dark:bg-gray-700 dark:text-white hover:text-black hover:shadow-lg hover:shadow-gray-500 dark:hover:shadow-gray-500 dark:hover:shadow-md dark:hover:text-sky-200 ease-in-out duration-300"
+					className="px-8 ml-4 py-2 bg-white text-gray-600 shadow-md rounded-lg dark:bg-gray-700 dark:text-white hover:text-black hover:shadow-lg hover:shadow-gray-500 dark:hover:shadow-gray-500 dark:hover:shadow-md dark:hover:text-sky-200 ease-in-out duration-300"
 					onClick={backHomeBtn}
 				>
 					<i className="fa fa-arrow-left mr-2"></i>Back
 				</button>
 			</div>
 			{state && (
-				<div className="container flex mx-auto p-8 pl-0 pr-0 desktop:mx-auto laptop:flex-row laptop:mx-5 tablet:flex-row mobile:flex-col mobile:place-items-center">
+				<div className="container flex mx-auto flex-col md:flex-row">
 					<img
 						src={state.flags.svg}
-						className="h-auto pr-8 desktop:w-1/2 laptop:w-3/4 tablet:w-2/5"
+						className="h-auto w-fit p-4 xl:w-1/2 md:w-2/5 md:h-96"
 						alt={state.flags.alt}
 					/>
-					<div className="p-8 pl-0">
-						<h2 className="font-bold text-2xl mb-8 laptop:text-2xl tablet:text-2xl mobile:text-xl">
+					<div className="p-8">
+						<h2 className="font-bold text-2xl mb-8 xl:text-2xl sm:text-2xl">
 							{state.name.official}
 						</h2>
-						<div className="grid grid-cols-2 gap-x-20 gap-y-4 desktop:grid-cols-2 tablet:grid-cols-2 mobile:grid-cols-1">
+						<div className="grid grid-cols-2 gap-x-20 gap-y-4 xl:grid-cols-2 sm:grid-cols-2">
 							<p>
 								Common name:{" "}
 								<span className="dark:text-gray-400 text-gray-700 text-sm">
