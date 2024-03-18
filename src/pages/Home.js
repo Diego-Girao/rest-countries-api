2d9cbe8d81a506d55ba9c04e924a8d1f4ad0392f
Diego <diegoramiro25@gmail.com>
2024-03-18 21:01:42 +0000

    style: change custom breakpoint to default and positions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,27 +37,39 @@ function Home(props) {
 
 	return (
 		<div className="bg-gray-100 dark:bg-gray-800 dark:text-white">
-			<div className="flex container mx-auto mb-16">
-				<i className="fa fa-search my-auto -mr-9 z-10 pr-2 pl-3 py-5 rounded-md text-gray-400"></i>
+			<div className="flex flex-col md:flex-row md:justify-between container mx-auto mb-4">
+				<i className="fa fa-search top-3 left-2 relative z-10 rounded-md text-gray-400"></i>
 				<input
 					type="search"
 					placeholder="Search for a country..."
-					className="pl-10 p-2 shadow-md rounded-md w-1/3 dark:bg-gray-700"
+					className="absolute truncate w-fit p-2 pl-10 shadow-md rounded-md md:w-1/3 dark:bg-gray-700"
 					onChange={(event) => searchCountry(event.target.value)}
 				/>
 				<select
-					className="ml-auto my-2 p-2 shadow-md rounded-md font-medium cursor-pointer dark:bg-gray-700"
+					className="my-8 p-2 shadow-md rounded-md font-medium cursor-pointer dark:bg-gray-700"
 					onChange={(val) => filterByRegion(val.target.value)}
 				>
-					<option value="">Filter by Region</option>
-					<option value="africa">Africa</option>
-					<option value="america">America</option>
-					<option value="asia">Asia</option>
-					<option value="europe">Europe</option>
-					<option value="oceania">Oceania</option>
+					<option className="text-xs md:text-base" value="">
+						Filter by Region
+					</option>
+					<option className="text-xs md:text-base" value="africa">
+						Africa
+					</option>
+					<option className="text-xs md:text-base" value="america">
+						America
+					</option>
+					<option className="text-xs md:text-base" value="asia">
+						Asia
+					</option>
+					<option className="text-xs md:text-base" value="europe">
+						Europe
+					</option>
+					<option className="text-xs md:text-base" value="oceania">
+						Oceania
+					</option>
 				</select>
 			</div>
-			<div className="grid gap-16 container mx-auto desktop:grid-cols-4 desktop:gap-4 tablet:grid-cols-2 tablet:place-items-center mobile:grid-cols-1 mobile:place-items-center">
+			<div className="grid gap-16 container mx-auto place-items-center xl:grid-cols-3 xl:px-8 md:grid-cols-2">
 				{countries.map((country, index) => (
 					<Link to="details" state={country} key={index}>
 						<ThumbDetails
